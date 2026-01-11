9baef0ff812000d035976b3312f4c4a6805d6105
Adekunle-1 <nellykun4u1999@gmail.com>
2026-01-11 22:27:40 +0100

    new update 11/01/26

--- First Proj - A guessing game using while loop.py
+++ First Proj - A guessing game using while loop.py
@@ -1,23 +1,26 @@
 import random
 
-print('this is a guessing game, you have 3 trials. There is a cash price of #100 if you can guess correctly. guess a number from 1 - 10').upper()
+print('this is a guessing game, you have 3 trials. There is a cash price of #100 if you can guess correctly. guess a number from 1 - 10'.upper())
 playgame = 'yes'
 
 while playgame == 'yes':
     numOfTrials = 3
     numToGuess = random.randrange(1, 10)
     while numOfTrials > 0:
-            userInput = int(input(f'Enter your pick here, you have {numOfTrials} left: '))
+            userInput = int(input(f'Enter your guess here, you have {numOfTrials} left: '))
             if userInput == numToGuess:
-                print ('Success')
+                print ('Congratulations!!!')
                 break
             else:
-                print ('try again')
+                if userInput > numToGuess:
+                    print ('Too high, try a lower number')
+                else:
+                    print ('Too low, try an higher number')
                 numOfTrials -= 1
 
     if numOfTrials == 0 and userInput != numToGuess:
-        print ('Game Over')
+        print ('Game Over.. Try Again')
         print (f'This is your secret number: {numToGuess}')
-    playgame =  input('DO you wanna play another game: yes or no')  
+    playgame =  input('DO you wanna play another game? enter yes to try again')  
 
 
