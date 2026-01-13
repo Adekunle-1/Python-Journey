38a0da08740249c07504528bb884d442b653df56
Adekunle-1 <nellykun4u1999@gmail.com>
2026-01-13 08:40:04 +0100

    update 13/01

--- First Proj - A guessing game (while loop + Function).py
+++ First Proj - A guessing game (while loop + Function).py
@@ -6,16 +6,23 @@ def play_game (NumOfTrials, max_num):
     NumToGuess = random.randrange(1, max_num + 1)
     
     while NumOfTrials > 0:
-        userInput = int(input(f'Enter your guess here. You have {NumOfTrials} left:\n  '.upper()))
+        try: 
+            userInput = int(input(f'Enter your guess here. You have {NumOfTrials} left:\n  '.upper()))
+        except ValueError:
+            print ('Please enter a valid number')
+            continue
 
         if userInput == NumToGuess:
-            print ('Congratulations!!!')
+            print ('Congratulations!!! YOU WON!!')
             return 'Win'
         
         if userInput > NumToGuess:
-            print ('Too high, try a lower number')
+            if userInput > max_num:
+                print (f'Incorrect, try a lower number. Maximum number should be {max_num}')
+            else:
+                print('Incorrect, try a lower number')
         else:
-            print ('Too low, try an higher number')
+            print ('Incorrect, try an higher number')
         
         NumOfTrials -= 1
 
@@ -25,10 +32,21 @@ def play_game (NumOfTrials, max_num):
 
 
 
-print ('The difficulty level is determined by the number of tries. Hard = 5, Medium = 4, Easy = 3'.upper())
-playgame = input('click any botton to begin, click q to quit\n'.upper())
+
+
+# game start, replay, and scroe count logic.
+print ('The difficulty level is determined by the number of tries.'.upper())
+print ('Hard = 5 tries, Medium = 4 tries, Easy = 3')
+playgame = input('click any button to begin, click q to quit\n'.upper())
+score = 0
+total_game = 0
+
 while playgame != 'q':
-    NumOfTrials = int(input('How many times to do you want to try? \n '))
+    try:
+        NumOfTrials = int(input('How many times do you want to try? \n '.upper()))
+    except ValueError:
+        print ('Please enter a valid number')
+        continue
     
     if NumOfTrials == 3:
         result = play_game (3, 10)
@@ -40,7 +58,14 @@ while playgame != 'q':
         print('incorrect selection, pick a number in 3, 4 and 5')
         continue
     
-    playgame = input('click any botton to begin this game, click q to quit\n'.upper())
+    if result == 'Win':
+        score += 1
+        total_game += 1
+    else:
+        score += 0
+        total_game += 1
+
+    print(f'Your score is {score}, and you have played a total of {total_game} game(s)')
+    playgame = input('click any botton to play again, click q to quit\n'.upper())
 
 print ('THANKS FOR PLAYING')
-    
